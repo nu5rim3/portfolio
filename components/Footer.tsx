ce43b6394ff99f65d30d10bb4444cf6a59aad1ff
Nusri Samath <nusrisamath@gmail.com>
2023-01-27 00:22:57 +0530

    updated social links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,25 +15,25 @@ export default function Footer() {
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.instagram.com/blaiti.codes" target="_blank" rel="noreferrer">
+                    <a href="https://www.instagram.com/nu5rim3" target="_blank" rel="noreferrer">
                         <Image src="/icons/instagram.svg" width={24} height={24} alt="instagram-icon" />
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.linkedin.com/in/skanderblaiti" target="_blank" rel="noreferrer">
+                    <a href="https://www.linkedin.com/in/nusri-samath/" target="_blank" rel="noreferrer">
                         <Image src="/icons/linkedin.svg" width={24} height={24} alt="linkedin-icon" />
                     </a>
                 </li>
                 <li>
-                    <a href="https://github.com/blaiti" target="_blank" rel="noreferrer">
+                    <a href="https://github.com/nu5rim3" target="_blank" rel="noreferrer">
                         <Image src="/icons/github.svg" width={24} height={24} alt="github-icon" />
                     </a>
                 </li>
-                <li>
-                    <a href="https://youtube.com/Blaiti" target="_blank" rel="noreferrer">
+                {/* <li>
+                    <a href="https://www.youtube.com/channel/UC8ainOoX4ZjOrtjYbP2LA1g" target="_blank" rel="noreferrer">
                         <Image src="/icons/youtube.svg" width={24} height={24} alt="youtube-icon" />
                     </a>
-                </li>
+                </li> */}
             </ul>
         </footer>
     )
