9bd7fbbef44b1e07fc80b4b2b36897c66488f804
Nusri Samath <nusrisamath@gmail.com>
2023-01-26 23:48:07 +0530

    Added cv link

--- components/Header.tsx
+++ components/Header.tsx
@@ -30,7 +30,7 @@ export default function Header() {
                             repeat={Infinity} // Repeat this Animation Sequence infinitely
                         /></h1>
                     <p className="gray">I design and code beautiful simple things, and I love what I do.</p>
-                    <a className="green" href="mailto:nusri.samath@gmail.com">Let&#39;s chat!</a>
+                    <a className="green" href="mailto:nusri.samath@gmail.com">Let&#39;s chat!</a><br/>
                     <a className="green" href="https://drive.google.com/file/d/1KhYUT-Ta-E0x8nVE_Kvs5Z-VeK8Xdqr_/view?usp=share_link">See more</a>
                 </div>
                 <Image src="/images/blaiti.png" width={463} height={513} alt="blaiti" />
