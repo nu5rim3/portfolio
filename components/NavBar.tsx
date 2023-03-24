68a7ffa700a03d4f1d7edcee6a0698377e8c9595
Nusri Samath <nusrisamath@gmail.com>
2023-03-24 16:49:10 +0530

    Updated bio url

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -11,7 +11,7 @@ export default function NavBar() {
 
             <ul className="flex white">
                 <li className="green">
-                    <a href="https://drive.google.com/file/d/1KhYUT-Ta-E0x8nVE_Kvs5Z-VeK8Xdqr_/view?usp=share_link" target="_blank" rel="noopener noreferrer">{"< Resume />"}</a>
+                    <a href="https://drive.google.com/file/d/1QRtoKt4Z1oWbFKWg1aOLgsBy_v8cDjr7/view?usp=sharing" target="_blank" rel="noopener noreferrer">{"< Resume />"}</a>
                 </li>
                 <li>
                     <Link href="/#about">About Me.</Link>
